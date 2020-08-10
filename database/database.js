d5430aa3c3cffdd99f92565bd2a62dbdceb77fb6
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-10 13:43:56 -0700

    installed esLint and fixed errors in one file

--- database/database.js
+++ database/database.js
@@ -1,37 +1,36 @@
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fec', {useNewUrlParser: true});
+
+mongoose.connect('mongodb://localhost/fec', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'database connection error:'));
-db.once('open', () => console.log('database is connected') );
+db.once('open', () => console.log('database is connected'));
 
-
-//open a connection to the database
+// open a connection to the database
 const picturesSchema = new mongoose.Schema({
   productId: Number,
   index: Number,
   fullSizeURL: String,
-  thumbnailURL: String
+  thumbnailURL: String,
 });
 
-//define the model
+// define the model
 const Picture = mongoose.model('Picture', picturesSchema);
 
-//gets records by productId
-const getPictures = (productId) => {
-  return new Promise((resolve, reject) => {
-    Picture.find({"productId": productId}).sort({index: 1})
-      .then(pictures => {
-        resolve(pictures);
-      })
-      .catch(err => {
-        reject(err);
-      })
-  })
-}
+// gets records by productId
+const getPictures = (productId) => new Promise((resolve, reject) => {
+  Picture.find({ productId }).sort({ index: 1 })
+    .then((pictures) => {
+      resolve(pictures);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
 
 module.exports = {
   db,
   getPictures,
-  Picture
+  Picture,
 };
