13ac6f2f11cc6c0f9d547fa989c6246eb976af3f
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-11 14:10:46 -0700

    wrote first puppeteer test

--- server/server.test.js
+++ server/server.test.js
@@ -1,5 +1,20 @@
 const sum = require('./sum.js');
 
-test('adds one and two to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// TODO: update this after merging in the window.location
+const baseURL = 'http://127.0.0.1:3001/';
+
+describe('sum', () => {
+  test('adds one and two to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+})
+
+describe('Main Carousel', () => {
+  beforeAll(async () => {
+    await page.goto(`${baseURL}`);
+  });
+
+  it('should be titled "Main Carousel"', async () => {
+    await expect(page.title()).resolves.toMatch('Main Carousel');
+  });
+})
