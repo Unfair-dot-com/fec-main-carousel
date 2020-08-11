e161ed7aebe5585374e0db0f73e98e40436e9620
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-10 21:46:00 -0700

    basic carousel loads onto page

--- client/index.jsx
+++ client/index.jsx
@@ -7,8 +7,9 @@ class App extends React.Component {
     super(props);
     this.state = {
       images: [],
-      productId: 1,
+      productId: 0,
     };
+    this.carouselLoader = this.carouselLoader.bind(this);
   }
 
   componentDidMount() {
@@ -21,22 +22,39 @@ class App extends React.Component {
       .catch(() => console.log('error getting pictures'));
   }
 
+  carouselLoader() {
+    if (this.state.images.length > 0) {
+      return this.state.images.map((image) => <img alt="Click Me!" id={image.index} className='thumbnail' src={image.thumbnailURL} />);
+    }
+    return <div />;
+  }
+
   render() {
     // sets a default value for the url, since state data
     // isn't available when the page first renders
     const imageURL = this.state.images.length > 0
       ? this.state.images[0].fullSizeURL : null;
 
-    // TODO replace this with a style sheet
+    // TODO replace these with a style sheet
     const imgStyle = {
       maxHeight: '500px',
     };
 
+    const carouselStyle = {
+      display: 'flex',
+      flexDirection: 'row',
+    };
+
+
     return (
       <div>
-        <div>
+        <div className="product-image">
           <img alt="Click to Zoom" src={imageURL} style={imgStyle} />
+        </div> <br/>
+        <div className="carousel">
+          {this.carouselLoader()}
         </div>
+
       </div>
     );
   }
