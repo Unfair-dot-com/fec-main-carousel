6c510fff59afb4951fa1d8a8fac4613f9d6fd557
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-11 11:43:54 -0700

    style cleanup

--- client/index.jsx
+++ client/index.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       images: [],
-      productId: 0,
+      productId: window.location.pathname.split('/')[2],
     };
   }
 
