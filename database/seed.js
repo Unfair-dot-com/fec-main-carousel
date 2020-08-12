2f9e44a785ad76b3cb5eca9d8bee0010409dad3d
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-11 20:30:47 -0700

    fixed style issues with seed.js

--- database/seed.js
+++ database/seed.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/* eslint-disable no-var */
 const db = require('./database.js').db;
 const Picture = require('./database.js').Picture;
 
@@ -13,34 +13,35 @@ const fileName = {
   fullSize: '',
   thumbnail: 'thumbnail',
   product0Full: 'sunMoon',
-  product0Thumbnail: 'sunMoon_thumbnail'
-}
+  product0Thumbnail: 'sunMoon_thumbnail',
+};
 
 var randNumGenerator = (min, max) => {
   return Math.floor((Math.random() * max) + min);
 }
 
-//randomize pictures
-//iterate through all productIDs, 1-99
-for (var id = 1; id < 100; id++ ) {
-  //choose a random number of pictures for the product, min 5, max 10 inclusive
-  var maxIndex = randNumGenerator(5, 11);
-  //then iterate through all indicies
-  for (var index = 0; index < maxIndex; index++ ) {
-    //generate a padded, random picture id between 1 and 40 inclusive
-    var paddedID = randNumGenerator(1, 41).toString().padStart(5,0);
-    //define a new document
-    var picture = new Picture({
-      'productId': id,
-      'index': index,
-      'fullSizeURL': `${baseURL}${folder.fullSize}${fileName.fullSize}${paddedID}.jpg`,
-      'thumbnailURL': `${baseURL}${folder.thumbnail}${fileName.thumbnail}${paddedID}.jpg`
+// randomize pictures
+// iterate through all productIDs, 1-99
+// eslint-disable-next-line no-plusplus
+for (let id = 1; id < 100; id++) {
+  // choose a random number of pictures for the product, min 5, max 10 inclusive
+  const maxIndex = randNumGenerator(5, 11);
+  // then iterate through all indicies
+  for (var index = 0; index < maxIndex; index++) {
+    // generate a padded, random picture id between 1 and 40 inclusive
+    const paddedID = randNumGenerator(1, 41).toString().padStart(5,0);
+    // define a new document
+    const picture = new Picture({
+      productId: id,
+      index,
+      fullSizeURL: `${baseURL}${folder.fullSize}${fileName.fullSize}${paddedID}.jpg`,
+      thumbnailURL: `${baseURL}${folder.thumbnail}${fileName.thumbnail}${paddedID}.jpg`,
     });
 
-    //save to the database
-    picture.save((err, picture) => {
+    // save to the database
+    picture.save((err) => {
       if (err) console.log('an error occurred writing to database: ', err);
-    })
+    }),
   }
 }
 
