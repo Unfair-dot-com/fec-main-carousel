3b3b9d77aca4739d1c205242c08c9c8ef1b6e16a
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-14 14:09:25 -0700

    clicking on a thumbnail changes the purple selector to that image

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,34 +1,60 @@
+/* eslint-disable no-console */
 import React from 'react';
 import Thumbnail from './Thumbnail';
+import Button from './Button';
 
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isActive: false,
+      activeThumbnail: 0,
     };
     this.carouselLoader = this.carouselLoader.bind(this);
+    this.handleThumbnailClick = this.handleThumbnailClick.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
   }
 
   carouselLoader() {
     // checks to see if images have loaded yet
     if (this.props.images.length > 0) {
+      const { activeThumbnail } = this.state;
       // if so, constructs image carousel
-      return this.props.images.map((image) => <Thumbnail alt="Click Me!" id={image.index} className="thumbnail" src={image.thumbnailURL} />);
+      return this.props.images.map((image) => (
+        <Thumbnail
+          id={image.index}
+          activeIndex={activeThumbnail}
+          src={image.thumbnailURL}
+          onClick={this.handleThumbnailClick}
+        />
+      ));
     }
     return <div />;
   }
 
+  // passes the index of the clicked thumbnail to state
+  handleThumbnailClick(e) {
+    this.setState({
+      activeThumbnail: Number(e.target.id),
+    });
+  }
+
+  handleButtonClick(e) {
+    console.log('button clicked!');
+  }
+
   render() {
-    // TODO replace these with a style sheet
+    // TODO replace these with a styled components
     const carouselStyle = {
       display: 'flex',
       flexDirection: 'row',
+      alignItems: 'center',
     };
 
     return (
       <div className="carousel" style={carouselStyle}>
+        <Button className='left-button' onClick={this.handleButtonClick}>&lt;</Button>
         {this.carouselLoader()}
+        <Button className='right-button' onClick={this.handleButtonClick}>&gt;</Button>
       </div>
     );
   }
--- client/components/Thumbnail.jsx
+++ client/components/Thumbnail.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
+// border prop compares activeIndex & id to see
+// whether it should have a purple border
 const Thumbnail = styled.img`
-  border: 2px solid #7f187f;
+  border: ${props => (props.activeIndex === props.id
+    ? '2px solid #7f187f'
+    : '2px solid white')};
   border-radius:10px;
+  margin: auto;
 `;
 
 export default Thumbnail;
