e2c10df88814ea9781e478b6a8fe56bbc2c6e435
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-18 19:56:07 -0700

    added additional static route to handle bundle.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static('public'));
 app.use('/products/', express.static('public'));
 app.use('/products/:productid', express.static('public'));
 
