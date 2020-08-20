5ecf6f31fdd71c4a66b00fbd51e093552d510139
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-20 16:34:58 -0700

    fixed issue with seed database

--- database/seed.js
+++ database/seed.js
@@ -20,14 +20,13 @@ var randNumGenerator = (min, max) => Math.floor((Math.random() * max) + min);
 
 // randomize pictures
 // iterate through all productIDs, 1-99
-// eslint-disable-next-line no-plusplus
-for (let id = 1; id < 100; id++) {
+for (let id = 1; id < 100; id += 1) {
   // choose a random number of pictures for the product, min 5, max 10 inclusive
   const maxIndex = randNumGenerator(5, 11);
   // then iterate through all indicies
   for (let index = 0; index < maxIndex; index += 1) {
     // generate a padded, random picture id between 1 and 40 inclusive
-    const paddedID = randNumGenerator(1, 41).toString().padStart(5, 0);
+    const paddedID = randNumGenerator(1, 40).toString().padStart(5, 0);
     // define a new document
     const picture = new Picture({
       productId: id,
